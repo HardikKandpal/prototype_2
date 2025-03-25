054ad97f7a5c0cad4f8b9d1abed5e7e8819fd58a
Hardik_Kandpal <hardikkandpal705@gmail.com>
2025-03-25 14:40:17 +0530

    commit

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,10 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  root: './src', // Ensure this points to your source directory
   build: {
-    outDir: './build', // Ensure this points to your desired output directory
-    rollupOptions: {
-      input: './src/main.jsx', // Ensure this matches your entry file
-    },
-  },
-});
+    outDir: 'dist',
+    assetsDir: 'assets',
+  }
+})
