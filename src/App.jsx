e91cae464585dd9757fb1b4ef94a3e6086e135e6
Hardik_Kandpal <hardikkandpal705@gmail.com>
2025-04-15 20:07:04 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import HomePage from './components/HomePage';
-import PropertyValuation from './components/PropertyValuation'; // Updated import path
-import PropertySearchPage from './components/PropertySearchPage';
+import PropertyValuation from './components/PropertyValuation';
+// Update the import for PropertySearchPage if the file exists with a different name
+// or create this component if it doesn't exist
+import PropertySearch from './components/PropertySearch'; // Changed from PropertySearchPage
 import MarketAnalysisPage from './components/MarketAnalysisPage';
 
 function App() {
@@ -13,8 +15,8 @@ function App() {
         <main>
           <Routes>
             <Route path="/" element={<HomePage />} />
-            <Route path="/valuation" element={<PropertyValuation />} /> {/* Updated component name */}
-            <Route path="/search" element={<PropertySearchPage />} />
+            <Route path="/valuation" element={<PropertyValuation />} />
+            <Route path="/search" element={<PropertySearch />} /> {/* Updated component name */}
             <Route path="/market-analysis" element={<MarketAnalysisPage />} />
           </Routes>
         </main>
