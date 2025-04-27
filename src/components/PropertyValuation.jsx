8c896aae05533a024a32e598380502b4c7031ca8
Hardik_Kandpal <122433038+HardikKandpal@users.noreply.github.com>
2025-04-27 18:37:26 +0530

    Update PropertyValuation.jsx

--- src/components/PropertyValuation.jsx
+++ src/components/PropertyValuation.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import { getPropertyValuation } from '../api';
-
+useEffect(() => {
+  console.log("[PropertyValuation.jsx] Component mounted");
+}, []);
 const PropertyValuation = () => {
   const [propertyDetails, setPropertyDetails] = useState({
     propertyType: '',
