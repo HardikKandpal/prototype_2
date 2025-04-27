0bba36579093ecef0f1eee60ec046e874f347bf0
Hardik_Kandpal <122433038+HardikKandpal@users.noreply.github.com>
2025-04-27 09:49:16 +0530

    Update MarketAnalysisPage.jsx

--- src/components/MarketAnalysisPage.jsx
+++ src/components/MarketAnalysisPage.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import { getMarketAnalysis, getLocations } from '../api';
+import { getMarketAnalysis, getLocations } from './lib/api';
 
 const MarketAnalysisPage = () => {
   const [locations, setLocations] = useState([]);
